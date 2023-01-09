c760cae1aa2dd21f8b342df9501b971ddbc3f982
Ramdhani11 <anwarramdhanie@gmail.com>
2023-01-09 09:25:31 +0700

    push test 7

---
+++ J. 620100.019.002(7)/source/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import SetupRouter from "./routes/SetupRouter";
+
+function App() {
+  return (
+    <div className="container">
+      <SetupRouter />
+    </div>
+  );
+}
+
+export default App;
